352572488dfdae7bdcf54040680d149c32c98087
Stevo99 <Sfailla1983@gmail.com>
2019-11-27 18:58:08 -0500

    added file for scroll function. imported scroll function into main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
+import { scrollElement } from './scroll.js';
+
 function loadThenAnimate() {
 	document.body.classList.add('bg-loading');
-
 	window.addEventListener('load', showPage, false);
 
 	function showPage() {
@@ -9,63 +10,131 @@ function loadThenAnimate() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+	// logic for delaying animations till after page loads
 	loadThenAnimate();
-
-	let backgroundImage = document.querySelector('header');
-
+	const backgroundImage = document.querySelector('header');
 	const observer = lozad(backgroundImage);
 	observer.observe();
+	// logic for all toggle events
+	const handleToggleEvents = () => {
+		const toggleMobileNav = () => {
+			const sideDraw = document.querySelector('.side-drawer');
+			const backdropToggle = document.querySelector(
+				'.toggle-backdrop'
+			);
+			const backdrop = document.querySelector('a');
+			const toggle = document.querySelector(
+				'.home__mobile-nav--target'
+			);
+			const mobileCheckBox = document.getElementById('checkbox');
+			const width = window.innerWidth;
 
-	const toggleMobileNav = () => {
-		let sideDraw = document.querySelector('.side-drawer');
-		let backdropToggle = document.querySelector('.toggle-backdrop');
-		let backdrop = document.querySelector('a');
-		let toggle = document.querySelector('.home__mobile-nav--target');
-		let mobileCheckBox = document.getElementById('checkbox');
-		let width = window.innerWidth;
+			const removeClass = () => {
+				sideDraw.classList.remove('open');
+				backdropToggle.classList.remove('backdrop');
+				mobileCheckBox.checked = false;
+			};
 
-		let removeClass = () => {
-			sideDraw.classList.remove('open');
-			backdropToggle.classList.remove('backdrop');
-			mobileCheckBox.checked = false;
+			if (sideDraw.classList.contains('open') && width > 799) {
+				sideDraw.addEventListener('change', removeClass);
+			}
+
+			backdrop.addEventListener('click', () => {
+				removeClass();
+			});
+
+			toggle.addEventListener('click', () => {
+				backdropToggle.classList.toggle('backdrop');
+				sideDraw.classList.toggle('open');
+				if (sideDraw.classList.contains('open')) {
+					mobileCheckBox.checked = false;
+				} else if (!sideDraw.classList.contains('open')) {
+					mobileCheckBox.checked = true;
+				}
+			});
 		};
 
-		if (sideDraw.classList.contains('open') && width > 799) {
-			sideDraw.addEventListener('change', removeClass);
-		}
+		const toggleAboutMe = () => {
+			const aboutCard = document.getElementById('about-card');
+			const aboutMeBtn = document.getElementById('about-me-button');
+			const aboutMeCloseBtn = document.getElementById('target');
 
-		backdrop.addEventListener('click', () => {
-			removeClass();
-		});
+			aboutMeBtn.addEventListener('click', () => {
+				if (aboutCard.classList.contains('closed')) {
+					aboutCard.classList.remove('closed');
+				}
+			});
 
-		toggle.addEventListener('click', () => {
-			backdropToggle.classList.toggle('backdrop');
-			sideDraw.classList.toggle('open');
-			if (sideDraw.classList.contains('open')) {
-				mobileCheckBox.checked = false;
-			} else if (!sideDraw.classList.contains('open')) {
-				mobileCheckBox.checked = true;
-			}
-		});
+			aboutMeCloseBtn.addEventListener('click', () => {
+				if (!aboutCard.classList.contains('closed')) {
+					aboutCard.classList.add('closed');
+				}
+			});
+		};
+
+		return [ toggleMobileNav(), toggleAboutMe() ];
 	};
-	toggleMobileNav();
+	// logic for all scroll effects
+	const handleScrollEffect = () => {
+		const scrollToAbout = () => {
+			let button = document.querySelectorAll('#about-button');
+			button.forEach(val => {
+				val.addEventListener('click', () => {
+					scrollElement(
+						document.querySelector('#about'),
+						1000,
+						'easeOutQuad'
+					);
+				});
+			});
+		};
 
-	const toggleAboutMe = () => {
-		let aboutCard = document.getElementById('about-card');
-		let aboutMeBtn = document.getElementById('about-me-button');
-		let aboutMeCloseBtn = document.getElementById('target');
+		const scrollToProject = () => {
+			let button = document.querySelectorAll('#project-button');
+			button.forEach(val => {
+				val.addEventListener('click', () => {
+					scrollElement(
+						document.querySelector('#projects'),
+						1000,
+						'easeOutQuad'
+					);
+				});
+			});
+		};
 
-		aboutMeBtn.addEventListener('click', () => {
-			if (aboutCard.classList.contains('closed')) {
-				aboutCard.classList.remove('closed');
-			}
-		});
+		const scrollToHome = () => {
+			let button = document.querySelectorAll('#home-button');
+			button.forEach(val => {
+				val.addEventListener('click', () => {
+					scrollElement(
+						document.querySelector('#home'),
+						1000,
+						'easeOutQuad'
+					);
+				});
+			});
+		};
 
-		aboutMeCloseBtn.addEventListener('click', () => {
-			if (!aboutCard.classList.contains('closed')) {
-				aboutCard.classList.add('closed');
-			}
-		});
+		const scrollToContact = () => {
+			let button = document.querySelectorAll('#contact-button');
+			button.forEach(val => {
+				val.addEventListener('click', () => {
+					scrollElement(
+						document.querySelector('#contact'),
+						1000,
+						'easeOutQuad'
+					);
+				});
+			});
+		};
+
+		return [
+			scrollToAbout(),
+			scrollToProject(),
+			scrollToHome(),
+			scrollToContact()
+		];
 	};
-	toggleAboutMe();
+
+	return [ handleScrollEffect(), handleToggleEvents() ];
 });
