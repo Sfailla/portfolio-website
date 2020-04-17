8ae5492590ac5490a2f5ad52f150b0ace8ef9253
Stevo99 <Sfailla1983@gmail.com>
2020-04-17 10:03:31 -0400

    changed from http server to https

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(CLIENT_DIR));
 
+if (process.env.NODE_ENV === 'production') {
+	//Serving the files on the CLIENT folder
+	app.use((req, res, next) => {
+		// if not an https server, the site will be redirected to https
+		if (req.header('x-forwarded-proto') !== 'https') {
+			res.redirect(`https://${req.header('host')}${req.url}`);
+		} else {
+			next();
+		}
+	});
+}
+
 app.get('*', (req, res) => {
 	res.sendFile(path.join(CLIENT_DIR, 'index.html'));
 });
