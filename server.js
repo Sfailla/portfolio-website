176124f37a7480de250bb5d2e6d0bb157256eeb3
Sfailla <Sfailla1983@gmail.com>
2021-01-09 12:55:39 -0500

    removed public folder and added src folder. moved files to src folder

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 const app = express();
 
-const DIST_DIR = path.join(__dirname, 'dist');
+const SOURCE_DIR = path.join(__dirname, 'src');
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(DIST_DIR));
+app.use(express.static(SOURCE_DIR));
 
 if (process.env.NODE_ENV === 'production') {
-  //Serving the files on the CLIENT folder
+  //Serving the files on the SOURCE folder
   app.use((req, res, next) => {
     // if not an https server, the site will be redirected to https
     if (req.header('x-forwarded-proto') !== 'https') {
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(DIST_DIR, 'index.html'));
+  res.sendFile(path.join(SOURCE_DIR, 'index.html'));
 });
 
 app.listen(PORT, () => {
