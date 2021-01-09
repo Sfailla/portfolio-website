adc70167ef69c2a1e68bf9013314918e1f10366a
Sfailla <Sfailla1983@gmail.com>
2021-01-09 11:38:39 -0500

    fixed path for dist folder in server.js file

--- server.js
+++ server.js
@@ -3,27 +3,27 @@ const path = require('path');
 
 const app = express();
 
-const CLIENT_DIR = path.join(__dirname, 'public');
+const DIST_DIR = path.join(__dirname, 'dist');
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(CLIENT_DIR));
+app.use(express.static(DIST_DIR));
 
 if (process.env.NODE_ENV === 'production') {
-	//Serving the files on the CLIENT folder
-	app.use((req, res, next) => {
-		// if not an https server, the site will be redirected to https
-		if (req.header('x-forwarded-proto') !== 'https') {
-			res.redirect(`https://${req.header('host')}${req.url}`);
-		} else {
-			next();
-		}
-	});
+  //Serving the files on the CLIENT folder
+  app.use((req, res, next) => {
+    // if not an https server, the site will be redirected to https
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    } else {
+      next();
+    }
+  });
 }
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(CLIENT_DIR, 'index.html'));
+  res.sendFile(path.join(DIST_DIR, 'index.html'));
 });
 
 app.listen(PORT, () => {
-	console.log(`listening on Port ${PORT}`);
+  console.log(`listening on Port ${PORT}`);
 });
