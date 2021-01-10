d765f76d7948c0f4b8b91f2ac1e52aae00e4dfd8
Sfailla <Sfailla1983@gmail.com>
2021-01-10 14:24:53 -0500

    removed scroll polyfill and went with native browser window scroll.  cleaned up js files

--- src/js/index.js
+++ src/js/index.js
@@ -1,99 +1,90 @@
 function loadThenAnimate() {
-	document.body.classList.add('bg-loading');
-	window.addEventListener('load', showPage, false);
+  document.body.classList.add('bg-loading');
+  window.addEventListener('load', showPage, false);
 
-	function showPage() {
-		document.body.classList.remove('bg-loading');
-	}
+  function showPage() {
+    document.body.classList.remove('bg-loading');
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	// delay animations till after page loads
-	loadThenAnimate();
-	const backgroundImage = document.querySelector('header');
-	const observer = lozad(backgroundImage);
-	observer.observe();
+  // delay animations till after page loads
+  loadThenAnimate();
+  const backgroundImage = document.querySelector('header');
+  const observer = lozad(backgroundImage);
+  observer.observe();
 
-	// scrolling functionality
-	const handleScrollEvents = () => {
-		const scrollToElement = (btn, el) => {
-			const buttons = document.querySelectorAll(btn);
-			const element = document.querySelector(el);
+  // scrolling functionality
+  (function handleScrollEvents() {
+    const scrollToElement = (btn, el) => {
+      const buttons = document.querySelectorAll(btn);
+      const element = document.querySelector(el);
 
-			buttons.forEach(button => {
-				button.addEventListener('click', () => {
-					element.scrollIntoView({ behavior: 'smooth' });
-				});
-			});
-		};
+      buttons.forEach(button => {
+        button.addEventListener('click', () => {
+          element.scrollIntoView({ behavior: 'smooth' });
+        });
+      });
+    };
 
-		const home = scrollToElement('#home-button', '#home');
-		const about = scrollToElement('#about-button', '#about');
-		const contact = scrollToElement('#contact-button', '#contact');
-		const projects = scrollToElement('#project-button', '#projects');
+    const home = scrollToElement('#home-button', '#home');
+    const about = scrollToElement('#about-button', '#about');
+    const contact = scrollToElement('#contact-button', '#contact');
+    const projects = scrollToElement('#project-button', '#projects');
 
-		return [ about, home, projects, contact ];
-	};
+    return [about, home, projects, contact];
+  })();
 
-	// toggle events
-	const handleToggleEvents = () => {
-		const toggleMobileNav = () => {
-			const sideDraw = document.querySelector('.side-drawer');
-			const backdropToggle = document.querySelector(
-				'.toggle-backdrop'
-			);
-			const backdrop = document.querySelector('a');
-			const toggle = document.querySelector(
-				'.home__mobile-nav--target'
-			);
-			const mobileCheckBox = document.querySelector('#checkbox');
-			const width = window.innerWidth;
+  // toggle events
 
-			const removeClass = () => {
-				sideDraw.classList.remove('open');
-				backdropToggle.classList.remove('backdrop');
-				mobileCheckBox.checked = false;
-			};
+  (function () {
+    const sideDraw = document.querySelector('.side-drawer');
+    const backdropToggle = document.querySelector('.toggle-backdrop');
+    const backdrop = document.querySelector('a');
+    const toggle = document.querySelector('.home__mobile-nav--target');
+    const mobileCheckBox = document.querySelector('#checkbox');
+    const width = window.innerWidth;
 
-			if (sideDraw.classList.contains('open') && width > 999) {
-				sideDraw.addEventListener('change', removeClass);
-			}
+    const removeClass = () => {
+      sideDraw.classList.remove('open');
+      backdropToggle.classList.remove('backdrop');
+      mobileCheckBox.checked = false;
+    };
 
-			backdrop.addEventListener('click', () => {
-				removeClass();
-			});
+    if (sideDraw.classList.contains('open') && width > 999) {
+      sideDraw.addEventListener('change', removeClass);
+    }
 
-			toggle.addEventListener('click', () => {
-				backdropToggle.classList.toggle('backdrop');
-				sideDraw.classList.toggle('open');
-				if (sideDraw.classList.contains('open')) {
-					mobileCheckBox.checked = false;
-				} else if (!sideDraw.classList.contains('open')) {
-					mobileCheckBox.checked = true;
-				}
-			});
-		};
+    backdrop.addEventListener('click', () => {
+      removeClass();
+    });
 
-		const toggleAboutMe = () => {
-			const aboutCard = document.querySelector('#about-card');
-			const aboutBtn = document.querySelector('#about-me-button');
-			const aboutCloseBtn = document.querySelector('#target');
+    toggle.addEventListener('click', () => {
+      backdropToggle.classList.toggle('backdrop');
+      sideDraw.classList.toggle('open');
+      if (sideDraw.classList.contains('open')) {
+        mobileCheckBox.checked = false;
+      } else if (!sideDraw.classList.contains('open')) {
+        mobileCheckBox.checked = true;
+      }
+    });
+  })();
 
-			aboutBtn.addEventListener('click', () => {
-				if (aboutCard.classList.contains('closed')) {
-					aboutCard.classList.remove('closed');
-				}
-			});
+  (function () {
+    const aboutCard = document.querySelector('#about-card');
+    const aboutBtn = document.querySelector('#about-me-button');
+    const aboutCloseBtn = document.querySelector('#target');
 
-			aboutCloseBtn.addEventListener('click', () => {
-				if (!aboutCard.classList.contains('closed')) {
-					aboutCard.classList.add('closed');
-				}
-			});
-		};
+    aboutBtn.addEventListener('click', () => {
+      if (aboutCard.classList.contains('closed')) {
+        aboutCard.classList.remove('closed');
+      }
+    });
 
-		return [ toggleMobileNav(), toggleAboutMe() ];
-	};
-
-	return [ handleToggleEvents(), handleScrollEvents() ];
+    aboutCloseBtn.addEventListener('click', () => {
+      if (!aboutCard.classList.contains('closed')) {
+        aboutCard.classList.add('closed');
+      }
+    });
+  })();
 });
