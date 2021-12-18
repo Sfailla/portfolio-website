bfe8793ed217bf64835e5a6deb0c934d7e23fbe5
Sfailla <Sfailla1983@gmail.com>
2021-12-18 14:01:50 -0500

    removed old projects.  changed about me content.  styling changes

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,11 +1,16 @@
-const imagemin = require('imagemin');
-const imageminWebp = require('imagemin-webp');
+const imagemin = require('imagemin')
+const imageminWebp = require('imagemin-webp')
 
-(async () => {
+/*
+  This function runs when the cache is initially created.
+  So to optimize a new image, first delete the cache then build
+*/
+
+;(async () => {
   await imagemin(['src/img/*.{jpeg,jpg,png}'], {
     destination: './src/img',
-    plugins: [imageminWebp({ quality: 50 })],
-  });
+    plugins: [imageminWebp({ quality: 50 })]
+  })
 
-  console.log('Images optimized');
-})();
+  console.log('Images optimized')
+})()
